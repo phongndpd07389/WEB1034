dac7354f5edcb4f426bf39b7fccd9a6f330ed102
PHONG <phongndpd07389@fpt.edu.vn>
2023-03-08 20:45:00 +0700

    lab1

--- lab1/app.js
+++ lab1/app.js
@@ -1,11 +1,8 @@
-//TODO
-let a = prompt("Moi ban nhap so nguyen a:");
-let a1 = parseInt(a);
+let a = prompt("Moi ban nhap so nguyen a:");        //nhập số nguyên từ bàn phím
+let a1 = parseInt(a);                               //chuyển chuỗi sang số nguyên
 let b = prompt("Moi ban nhap so nguyen b:");
 let b1 = parseInt(b);
-// let c = a1 + b1;
-let c = parseInt(a1) + parseInt(b1);
-let x = a + b;
-document.write(c);
-console.log(c);
-document.write(x);
+// C1: let c = a1 + b1;
+let c = parseInt(a1) + parseInt(b1);                //C2
+document.write(c);                                  //In ra màn hình web
+console.log(c);                                     //In ra console
