5426c6c1718dd97a372b0cc3d817a6ca301549d7
PHONG <phongndpd07389@fpt.edu.vn>
2023-03-08 20:20:16 +0700

    first commit

---
+++ lab1/app.js
@@ -0,0 +1,11 @@
+//TODO
+let a = prompt("Moi ban nhap so nguyen a:");
+let a1 = parseInt(a);
+let b = prompt("Moi ban nhap so nguyen b:");
+let b1 = parseInt(b);
+// let c = a1 + b1;
+let c = parseInt(a1) + parseInt(b1);
+let x = a + b;
+document.write(c);
+console.log(c);
+document.write(x);
