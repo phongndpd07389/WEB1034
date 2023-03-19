c529f5bf67604d377aabf9f80c5e3ebd16662ce0
PHONG <phongndpd07389@fpt.edu.vn>
2023-03-20 00:27:31 +0700

    bai2lab3

---
+++ lab3/app.js
@@ -0,0 +1,50 @@
+while(true){
+    do{
+        var diemT = prompt("Nhap diem Toan");
+        diemT = parseFloat(diemT);
+    }while(diemT < 0 || diemT > 10);
+    do{
+        var diemL = prompt("Nhap diem Ly");
+        diemL = parseFloat(diemT);
+    }while(diemL < 0 || diemL > 10);
+    do{
+        var diemH = prompt("Nhap diem Hoa");
+        diemH = parseFloat(diemT);
+    }while(diemH < 0 || diemH > 10);
+    do{
+        var diemS = prompt("Nhap diem Sinh");
+        diemS = parseFloat(diemT);
+    }while(diemS < 0 || diemS > 10);
+    var diemTB = (diemT + diemL + diemH + diemS)/4;
+    console.log("1. Tinh diem trung binh(switch case");
+    console.log("2. Tinh diem trung binh(if else");
+    console.log("0. Thoat");
+    var chon = prompt("Nhap lua chon");
+    switch(chon){
+        case '1':
+            switch((((int)(diemTB*10))-10)/20){
+                case 1:
+                    console.log("Yeu");
+                case 2:
+                    console.log("Yeu");
+                case 3:
+                    console.log("Yeu");
+                case 4:
+                    console.log("Yeu");           
+            }
+            break;
+        case '2':
+            if(diemTB <= 10 && diemTB >= 9){
+                console.log("Gioi");
+            }else if(diemTB >= 7){
+                console.log("Kha");
+            }else if(diemTB >= 5){
+                console.log("Trung binh");
+            }else{
+                console.log("Yeu");
+            }
+            break;
+        case '0':
+            break;
+    }
+}
